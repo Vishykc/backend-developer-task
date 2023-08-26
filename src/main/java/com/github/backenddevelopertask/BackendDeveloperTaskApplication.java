19cc20eb91f130c2ed37d3fb254628d1f36e6e6a
Viktor Vranar <viktor.vranar@gmail.com>
2023-08-26 13:27:24 +0200

    dodani inicijalni spring initializr projekt i upute za zadatak

---
+++ src/main/java/com/github/backenddevelopertask/BackendDeveloperTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.backenddevelopertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeveloperTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeveloperTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/backenddevelopertask/BackendDeveloperTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.backenddevelopertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeveloperTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
